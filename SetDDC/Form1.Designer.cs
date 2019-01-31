24a15dff5e12a6ea2545adb19fcdb55ccc9b8b0b
David Miller <simsaladoo@hotmail.com>
2019-01-30 16:40:31 -0800

    reporting of confirmed DDC added
    Display now shows when a proper DDC key has been found.  Next step is to add one if not there.

--- SetDDC/Form1.Designer.cs
+++ SetDDC/Form1.Designer.cs
@@ -30,81 +30,104 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.button2_SetSharedDDC = new System.Windows.Forms.Button();
+            this.button3_SetLocalDDC = new System.Windows.Forms.Button();
+            this.button4_OpenSys = new System.Windows.Forms.Button();
+            this.button5_readENV = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semilight", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.button1.Location = new System.Drawing.Point(21, 215);
+            this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(151, 41);
             this.button1.TabIndex = 0;
             this.button1.Text = "Refresh";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // richTextBox1
             // 
             this.richTextBox1.BackColor = System.Drawing.Color.Black;
             this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI Semilight", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Location = new System.Drawing.Point(151, 0);
             this.richTextBox1.Margin = new System.Windows.Forms.Padding(6);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
-            this.richTextBox1.Size = new System.Drawing.Size(801, 193);
+            this.richTextBox1.Size = new System.Drawing.Size(650, 411);
             this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "Started...";
+            this.richTextBox1.Text = "";
             // 
-            // button2
+            // button2_SetSharedDDC
             // 
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.button2.Location = new System.Drawing.Point(221, 215);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(151, 41);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Set Shared DDC";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2_SetSharedDDC.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2_SetSharedDDC.Font = new System.Drawing.Font("Segoe UI Semilight", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2_SetSharedDDC.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button2_SetSharedDDC.Location = new System.Drawing.Point(0, 80);
+            this.button2_SetSharedDDC.Name = "button2_SetSharedDDC";
+            this.button2_SetSharedDDC.Size = new System.Drawing.Size(151, 41);
+            this.button2_SetSharedDDC.TabIndex = 2;
+            this.button2_SetSharedDDC.Text = "Set Shared DDC";
+            this.button2_SetSharedDDC.UseVisualStyleBackColor = true;
+            this.button2_SetSharedDDC.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // button3_SetLocalDDC
             // 
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.button3.Location = new System.Drawing.Point(425, 215);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(151, 41);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Use Local DDC";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3_SetLocalDDC.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3_SetLocalDDC.Font = new System.Drawing.Font("Segoe UI Semilight", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3_SetLocalDDC.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button3_SetLocalDDC.Location = new System.Drawing.Point(0, 120);
+            this.button3_SetLocalDDC.Name = "button3_SetLocalDDC";
+            this.button3_SetLocalDDC.Size = new System.Drawing.Size(151, 41);
+            this.button3_SetLocalDDC.TabIndex = 3;
+            this.button3_SetLocalDDC.Text = "Use Local DDC";
+            this.button3_SetLocalDDC.UseVisualStyleBackColor = true;
+            this.button3_SetLocalDDC.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
+            // button4_OpenSys
             // 
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.ForeColor = System.Drawing.Color.Red;
-            this.button4.Location = new System.Drawing.Point(630, 215);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(151, 41);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Open System";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4_OpenSys.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4_OpenSys.Font = new System.Drawing.Font("Segoe UI Semilight", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4_OpenSys.ForeColor = System.Drawing.Color.Red;
+            this.button4_OpenSys.Location = new System.Drawing.Point(0, 370);
+            this.button4_OpenSys.Name = "button4_OpenSys";
+            this.button4_OpenSys.Size = new System.Drawing.Size(151, 41);
+            this.button4_OpenSys.TabIndex = 4;
+            this.button4_OpenSys.Text = "Open System";
+            this.button4_OpenSys.UseVisualStyleBackColor = true;
+            this.button4_OpenSys.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5_readENV
+            // 
+            this.button5_readENV.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5_readENV.Font = new System.Drawing.Font("Segoe UI Semilight", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5_readENV.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.button5_readENV.Location = new System.Drawing.Point(0, 40);
+            this.button5_readENV.Name = "button5_readENV";
+            this.button5_readENV.Size = new System.Drawing.Size(151, 41);
+            this.button5_readENV.TabIndex = 5;
+            this.button5_readENV.Text = "Show Env Variables";
+            this.button5_readENV.UseVisualStyleBackColor = true;
+            this.button5_readENV.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(800, 275);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(800, 411);
+            this.Controls.Add(this.button5_readENV);
+            this.Controls.Add(this.button4_OpenSys);
+            this.Controls.Add(this.button3_SetLocalDDC);
+            this.Controls.Add(this.button2_SetSharedDDC);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI Semilight", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "Form1";
@@ -119,9 +142,10 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2_SetSharedDDC;
+        private System.Windows.Forms.Button button3_SetLocalDDC;
+        private System.Windows.Forms.Button button4_OpenSys;
+        private System.Windows.Forms.Button button5_readENV;
     }
 }
 
--- SetDDC/Form1.cs
+++ SetDDC/Form1.cs
@@ -18,29 +18,99 @@ namespace SetDDC
             InitializeComponent();
         }
 
+
+
+
+        //startup
         private void Form1_Load(object sender, EventArgs e)
         {
-            // Initial Load -=- read the current DDC envionroment path if it exists
-            
-            IDictionary environmentVariables = System.Environment.GetEnvironmentVariables();
-            foreach (DictionaryEntry de in environmentVariables)
-            {
-               
-                richTextBox1.Text += Environment.NewLine + "Key: " + de.Key + " Value: " + de.Value;
+            richTextBox1.Clear();
+            richTextBox1.Focus();
+            richTextBox1.Text += "Started...";
+            richTextBox1.Text += Environment.NewLine + "Choose wisely";
 
-            }
 
-              
-         
         }
 
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // set local ddc (removes any ue4 varialbes
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // set shared ddc
+            //should prompt for DDC location
+
+
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // read variables
+            richTextBox1.Text += Environment.NewLine + "Reading Environment Variables...";
+            ReadEnv();
+            
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // refresh? i dunno
+            richTextBox1.Clear();
+            richTextBox1.Focus();
+            richTextBox1.Text += "Refreshed...";
+
+        }
+
+
+        // manually open the system properties window so user can edit/check variables
         private void button4_Click(object sender, EventArgs e)
         {
             string strCmdText = ("SystemPropertiesAdvanced.exe");
             System.Diagnostics.Process.Start(strCmdText);
-            
-            
+
         }
+
+
+
+
+
+        // Reads back all the current varialbes
+        public void ReadEnv ()
+        {
+            // Initial Load -=- read the current DDC envionroment path if it exists
+
+            IDictionary environmentVariables = System.Environment.GetEnvironmentVariables();
+            foreach (DictionaryEntry de in environmentVariables)
+            {
+
+                //richTextBox1.Text += Environment.NewLine + "Key: " + de.Key + " Value: " + de.Value;
+                string UE4CheckValue = de.Key.ToString();
+                if (UE4CheckValue == "UE-SharedDataCachePath")
+                {
+                    richTextBox1.Text += Environment.NewLine + "Found an Unreal DDC environment:";
+                    richTextBox1.Text += Environment.NewLine + "Key: " + de.Key;
+                    richTextBox1.Text += Environment.NewLine + "Value: " + de.Value;
+                    Console.WriteLine("End of entry");
+                }
+                else
+                {
+                    //richTextBox1.Text += Environment.NewLine + "Key: " + de.Key + " Value: " + de.Value;
+                    Console.WriteLine("End of entry");
+                }
+
+            }
+        }
+
+
+        
     }
     
 }
